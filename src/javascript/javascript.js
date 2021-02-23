d0dc8b464272887d4968b330108c4beb51f294ca
luuciarra <luuciarra@gmail.com>
2021-02-23 15:17:06 -0300

    Correcciones js

--- src/javascript/javascript.js
+++ src/javascript/javascript.js
@@ -1,10 +1,33 @@
-let burger = document.querySelector('.burger');
-let cross = document.querySelector('.close');
-let menu = document.querySelector('.side-nav');
+// let burger = document.querySelector('.burger');
+// let cross = document.querySelector('.close');
+// let menu = document.querySelector('.side-nav');
 
-burger.addEventListener('click', function(){
-    menu.style.transform = 'translateX(0%)';
-})
-cross.addEventListener('click', function(){
-    menu.style.transform = 'translateX(-200%)';
-})
+// burger.addEventListener('click', function(){
+//     menu.style.transform = 'translateX(0%)';
+// })
+// cross.addEventListener('click', function(){
+//     menu.style.transform = 'translateX(-200%)';
+// })
+
+
+var burger =  $(".burger") //document.querySelector('.burger');
+var cross =  $(".close") //document.querySelector('.close');
+var menu =  $(".side-nav") //document.querySelector('.side-nav');
+
+function toggleMenu()
+{
+    menu.toggleClass("opened");
+}
+
+burger.on("click", toggleMenu);
+cross.on("click", toggleMenu);
+
+// burger.on("click",() => {
+//     menu.addClass("opened");
+//     // menu.style.transform = 'translateX(0%)';
+// });
+
+// cross.on("click", () => {
+//     menu.removeClass("opened");
+//     // menu.style.transform = 'translateX(-200%)';
+// })
